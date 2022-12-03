a934c55b93606be2456befdcb5ed30dbef77082a
qaasimahmad <abumaahir1@gmail.com>
2022-12-04 00:50:28 +0100

    Updates

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -15,7 +15,7 @@ const Blog = () => {
         </div>
         <div className="blog-container-groupB">
           <Article blogImage={blog07} date="March 15, 2022" title="ZRANK is your infrastructure open to hybrid working?" />
-          <Article blogImage={blog08} date="April 26, 2022" title="ZRANK Learn about SRE and its role in your business scalability?" />
+          <Article blogImage={blog08} date="April 26, 2022" title="ZRANK Learn about SRE and its role in your business scalability" />
           <Article blogImage={blog04} date="July 18, 2022" title="ZRANK and Open  AI is the future. Let us explore how it is?" />
           <Article blogImage={blog05} date="Dec 02, 2022" title="ZRANK IT trends in 2023 and future technology adoption for your business?" />
         </div>
